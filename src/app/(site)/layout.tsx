eb24e48045e2dd0e50adb1ccd241e7abfb57ae61
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2025-10-12 10:16:50 +0400

    add page transition using Gsap

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import PageLayout from "@/components/page-layout/page-layout";
 import PageHeader from "@/components/page-layout/page-header";
 import PageFooter from "@/components/page-layout/page-footer";
 import PageContent from "@/components/page-layout/page-content";
+import PageTransition from "@/components/page-transition";
 
 interface SiteLayoutProps {
   children: React.ReactNode;
@@ -9,10 +10,12 @@ interface SiteLayoutProps {
 
 export default function SiteLayout({ children }: SiteLayoutProps) {
   return (
-    <PageLayout>
-      <PageHeader>Header</PageHeader>
-      <PageContent>{children}</PageContent>
-      <PageFooter>Footer</PageFooter>
-    </PageLayout>
+    <PageTransition>
+      <PageLayout>
+        <PageHeader>Header</PageHeader>
+        <PageContent>{children}</PageContent>
+        <PageFooter>Footer</PageFooter>
+      </PageLayout>
+    </PageTransition>
   );
 }
