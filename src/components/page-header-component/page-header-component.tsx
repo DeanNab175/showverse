5f0d88268811bd6ebf3ea67019a23a0f609e4eb8
satyajit.nabeebacus <satyajit.nabeebacus@rapp.com>
2025-12-23 17:58:18 +0400

    update the button component and add link to contact button in page header

--- src/components/page-header-component/page-header-component.tsx
+++ src/components/page-header-component/page-header-component.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 function PageHeaderComponent() {
   return (
     <section className="flex justify-between items-center pb-4">
       <div className="logo">
-        <h1 className="text-lg font-bold">ShowVerse</h1>
+        <h1 className="text-lg">
+          Show<span className="text-primary font-bold">Verse</span>
+        </h1>
       </div>
       <div className="flex gap-4 items-center">
         <Button
@@ -14,11 +19,13 @@ function PageHeaderComponent() {
         >
           <i className="text-xl icon-moon"></i>
         </Button>
-        <Button>
-          Contact me
-          <span>
-            <i className="icon-send"></i>
-          </span>
+        <Button asChild>
+          <Link href="/contact" className="menu-nav-link">
+            Contact me
+            <span className="text-sm">
+              <i className="icon-send font-bold"></i>
+            </span>
+          </Link>
         </Button>
       </div>
     </section>
