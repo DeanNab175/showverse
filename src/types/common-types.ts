0c384dc9e488bb45d62581876992670c64ea0c8b
Satyajit Nabeebacus <satyajit.nabeebacus@omc.com>
2026-06-17 20:20:17 +0400

    add common data type for pages data

---
+++ src/types/common-types.ts
@@ -0,0 +1,18 @@
+import type { HeadingLevelType } from "./typography-types";
+
+export type HeadingType = {
+  class?: string;
+  text: string;
+  level: HeadingLevelType;
+};
+
+export type BaseSectionType<TContent extends object = object> = {
+  id: string;
+  name: string;
+  wrapperClass?: string;
+  sectionClass?: string;
+  content: {
+    wrapperClass?: string;
+    heading?: HeadingType;
+  } & TContent;
+};
